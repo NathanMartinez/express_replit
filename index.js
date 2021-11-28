5434830f42073a02f80c89cab0ab06b73d2249c3
Nate Martinez <natedzmtz@yahoo.com>
2021-11-28 19:12:12 +0000

    Adjusting some code

--- index.js
+++ index.js
@@ -9,20 +9,29 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("tiny"));
-app.use(express.static('public'));
+
+
+const path = require('path')
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 const port = 3000;
 
 const Database = require("@replit/database")
 const db = new Database()
 
+
+// Get Requests
 app.get('/', (req, res) => {
-	res.sendFile(process.cwd() + '/public/index.html');
+
 })
+
+// Users Endpoint
 app.get('/users', (req, res) => {
-	// res.sendFile(process.cwd() + '/public/index.html');
-	res.send('Hello World!')
+	res.send('Test')
 })
+
+// Post
 
 app.post("/users", function(req, res) {
 	console.log(req.body)
